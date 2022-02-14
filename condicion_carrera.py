c0259c3ba8af5eae987f236769e7c1f91312f613
Uziel <201274@ids.upchiapas.edu.mx>
2022-02-14 12:47:36 -0600

    initial commit

---
+++ condicion_carrera.py
@@ -0,0 +1,34 @@
+import threading 
+import time
+result = 2
+  
+
+
+def operacion(): 
+    global result
+    result = result*result
+  
+
+def resultados(lock): 
+    global result
+    lock.acquire() 
+    operacion() 
+    print(result) 
+    lock.release() 
+  
+
+def main(): 
+   
+    for y in range(5):
+        print(y)
+        lock = threading.Lock() 
+        sub_proceso = threading.Thread(target=resultados, args=(lock,)) 
+        sub_proceso.start() 
+        sub_proceso.join() 
+    time.sleep(2)
+   
+  
+if __name__ == "__main__": 
+    main()
+    
+    
